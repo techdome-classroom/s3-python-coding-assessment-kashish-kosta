e4c4bc4d11b916054049b692009f32cba0acd330
kashish-kosta <kashishkosta04@gmail.com>
2024-09-30 15:23:29 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,30 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+   def roman_to_integer(s: str) -> int:
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    n = len(s)
 
+    for i in range(n):
+        value = roman_values[s[i]]
+
+        if i + 1 < n and roman_values[s[i + 1]] > value:
+            total -= value
+        else:
+            total += value
+    
+    return total
+
+print(roman_to_integer("III"))      
+print(roman_to_integer("LVIII"))    
+print(roman_to_integer("MCMXCIV"))  
 
 
